a4ecb449a30201f244146dc34debca9eaa1558ff
KelvinMvungi <kelvin.mvungi01@gmail.com>
2023-10-04 01:29:43 +0300

    Test therepeat function first time

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
-
+const str = 'hey';
+const num = 3;
+const repeatString = function(str, num) {
+let res = "";
+let i = 0;
+do {
+    if (i < 0) {
+        return "ERROR";
+    }
+    res += str;
+    i++;
+} while( i < num)
+return res;
+}
 // Do not edit below this line
 module.exports = repeatString;
