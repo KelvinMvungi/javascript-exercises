bf9371026848a099edc92aac43d281eb668d97dc
KelvinMvungi <kelvin.mvungi01@gmail.com>
2023-10-06 00:48:47 +0300

    Test sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+   if(typeof a !== 'number' || typeof b !== 'number' || a < 0 || b < 0) return 'ERROR';
 
-};
-
+   const argsArr = [a, b]
+    let sum = 0;
+    for(let i = Math.max(...argsArr); i >= Math.min(...argsArr); i--){
+        sum += i;
+    }
+    return sum;
+    }
 // Do not edit below this line
 module.exports = sumAll;
