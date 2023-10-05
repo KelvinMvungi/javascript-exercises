ab14e503c6c42e5e2b2035de29fd55aab7fb5a93
KelvinMvungi <kelvin.mvungi01@gmail.com>
2023-10-05 23:09:19 +0300

    Test the removeFromArry function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(arr, ...args) {
+ const set2 = new Set(args);
+ const result = arr.filter(item => !set2.has(item));
+ return result;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
