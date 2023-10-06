b6efd57f8953f61a42e9a0c21b70a12432b42d97
KelvinMvungi <kelvin.mvungi01@gmail.com>
2023-10-07 00:05:26 +0300

    Test the fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+  if( (typeof index !== 'number' && typeof index !== 'string') || (typeof index === 'string' && !/^\d+$/.test(index)) || index < 0 || !Number.isInteger(Number(index))) {
+    return "OOPS";
+  }
+  if (index === 0) {
+    return 0;
+  } else if (index === 1) {
+    return 1;
+  } else {
+    let prev = 0;
+    let current = 1;
+    for (let i = 2; i <= index; i++) {
+      const next = prev + current;
+      prev = current;
+      current = next;
+    }
+    return current;
+  }
 };
 
 // Do not edit below this line
