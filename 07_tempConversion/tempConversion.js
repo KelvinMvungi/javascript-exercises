059813357e46751cf96deda0f68e47633acdfedb
KelvinMvungi <kelvin.mvungi01@gmail.com>
2023-10-06 12:28:01 +0300

    Test the tempConversion function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (fahrenheit - 32) * (5/9);
+  const celsiusToOneDecimal = Math.round(celsius * 10) / 10
+  return celsiusToOneDecimal;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+ let fahrenheit = celsius * (9/5) + 32;
+ const fahrenheitToOneDecimal = Math.round(fahrenheit * 10) / 10;
+  return fahrenheitToOneDecimal;
 };
 
 // Do not edit below this line
