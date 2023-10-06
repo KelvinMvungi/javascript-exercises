918a4fbcd14285d46cb2cc75b214f09ba535d2cc
KelvinMvungi <kelvin.mvungi01@gmail.com>
2023-10-06 23:22:28 +0300

    Test the palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    const reversedStr = str.split("").reverse().join("");
 
+    return str === reversedStr;
 };
 
 // Do not edit below this line
