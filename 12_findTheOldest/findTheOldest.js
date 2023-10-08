3810e0d2084d4bac64b4b3f9dec4e1c140cebc91
KelvinMvungi <kelvin.mvungi01@gmail.com>
2023-10-08 23:21:51 +0300

    Test the findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    if (people.length === 0) {
+        return null; 
+      }
+    
+      let personWithGreatestAge = people[0];
+      const currentYear = new Date().getFullYear();
+    
+      for (let i = 1; i < people.length; i++) {
+        const currentPerson = people[i];
+        let currentAge;
 
+        if (currentPerson.yearOfDeath){
+            currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+        } else {
+            currentAge = currentYear - currentPerson.birthYear;
+        }
+        
+        
+        const oldestAge = personWithGreatestAge.yearOfDeath ? personWithGreatestAge.yearOfDeath - personWithGreatestAge.yearOfBirth : currentYear - personWithGreatestAge.yearOfBirth;
+        
+        if (currentAge > oldestAge) {
+          personWithGreatestAge = currentPerson
+        }
+      }
+    
+      return personWithGreatestAge;
 };
 
 // Do not edit below this line
